e39fc7056384e70a9d93d5e76d204691fd91f10a
Wilder Lizama Garcia <wilderlizama@gmail.com>
2019-10-30 11:20:22 -0500

    Localstorage

---
+++ src/localstorage.js
@@ -0,0 +1,11 @@
+export const obtenerStageStorage = () => {
+    const citasStorage = localStorage.getItem('citas')
+    if (citasStorage === null)
+        return undefined
+    return JSON.parse(citasStorage)
+}
+
+export const guardarStateStorage = state => {
+    const citasState = JSON.stringify(state)
+    localStorage.setItem('citas', citasState)
+}
--- src/store.js
+++ src/store.js
@@ -1,10 +1,19 @@
 import { createStore } from 'redux'
 import reducer from './reducers'
+import { obtenerStageStorage, guardarStateStorage } from './localstorage'
 
+const storageState = obtenerStageStorage()
 
 export const store = createStore(
     reducer,
+    storageState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // desarrollo
 )
+
+store.subscribe( () => {
+    guardarStateStorage({
+        citas: store.getState().citas
+    })
+})
 
 export default store
